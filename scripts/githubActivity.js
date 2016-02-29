3a6984ade6859befa5fb54883add503c2bf0bbd3
EduardoMY <eduardo.medina@cetys.edu.mx>
2016-02-28 19:34:21 -0800

    Now you can see my repos

--- scripts/githubActivity.js
+++ scripts/githubActivity.js
@@ -1,3 +1,5 @@
+var informationRepos=[];
+
 function loadRepositories(){ 
 $.ajax({
     type: "GET",
@@ -5,15 +7,74 @@ $.ajax({
     dataType: "json",
     success: function(result){
         for(i in result){
+            var information=[];
+            information[1]=result[i].html_url;
+            information[1]=result[i].name;
+            information[2]=result[i].description;
+            information[3]=result[i].created_at;
+            
             $("#github-projects").append(
-              "<article class='github-repos'><a href='" + result[i].html_url + "' target='_blank'>" +
-              result[i].name + "</a></article>"
+              printIndividualRepos(information[0],
+              information[1],information[2], information[3])
             )
+            informationRepos.push(information);
         }
     }   
 
 })
 };
+
+function printIndividualRepos(url,name, description,date ){
+             return "<div class='github-repos-wrapper'><article class='github-repos'><a href='" + url + 
+              "' target='_blank'>" +
+              name + "</a><hr>"+
+              description + "<hr>"+
+              date +"</article></div>"
+}
+
+function sortName(){
+    var temp=[];
+    $("#github-projects").empty();
+    
+    for(var i=0; i<informationRepos.length; i++){
+        for(var j=i+1; j<informationRepos.length; j++)
+            if(informationRepos[j,1]<informationRepos[i, 1])
+                {
+                    temp=informationRepos[i];    
+                    informationRepos[i]=informationRepos[j];
+                    informationRepos[j]=temp;
+                }
+             $("#github-projects").append(
+              printIndividualRepos(
+                  informationRepos[i, 0],
+                  informationRepos[i, 1],
+                  informationRepos[i, 2],
+                  informationRepos[i, 3])
+            );   
+    }
+}
+function sortDate(){
+    var temp=[];
+    $("#github-projects").empty();
+    
+    for(var i=0; i<informationRepos.length; i++){
+        for(var j=i+1; j<informationRepos.length; j++)
+            if(informationRepos[j,3]<informationRepos[i, 3])
+                {
+                    temp=informationRepos[i];    
+                    informationRepos[i]=informationRepos[j];
+                    informationRepos[j]=temp;
+                }
+             $("#github-projects").append(
+              printIndividualRepos(
+                  informationRepos[i, 0],
+                  informationRepos[i, 1],
+                  informationRepos[i, 2],
+                  informationRepos[i, 3])
+            );   
+    }
+}
+
 function loadAvatar(){
     $.ajax(
         {
