e1f0d4ddb44ff1588c8b5f7ffef20fd9abe67f08
EduardoMY <eduardo.medina@cetys.edu.mx>
2016-04-01 20:06:54 -0700

    A more useful example

--- scripts/foxForceAnalysis.js
+++ scripts/foxForceAnalysis.js
@@ -5,25 +5,36 @@ onload=function openXMLFIle(){
 	if(xhttp.readyState==4 && xhttp.status==200){
 	    myFunction(xhttp);
 	}
-	xhttp.close();
+	//xhttp.close();
     };
     xhttp.open("GET", "data/data.xml", true);
-    xhttp.send();
+    xhttp.send(null);
 }
 
 function myFunction(xml){
     var xmlDoc=xml.responseXML;
     var data=xmlDoc.getElementsByTagName("PLP")[0];
     var nodeList=data.firstChild;
-    getValues(nodeList);
+    getValuesPLP(nodeList);
 }
 
-function getValues(nodeList){
-    //document.write("Hey");
+function getValuesPLP(nodeList){
+    var values=[];
+    var vLength;
+    var points=[];
     while(nodeList != null){
 	if(nodeList.nodeType==1)
-	    document.write(nodeList.firstChild.nodeValue);
+	    values.push(nodeList.firstChild.nodeValue);
 	nodeList=nodeList.nextSibling;
     };
-//    xhttp.close();
+    vLength=values.length;
+    for(c=0; c<vLength/2; c++){
+	points.push({x:values[c], y:values[c+vLength/2]});
+	document.write(points[c].x+""+points[c].y+"br");
+    }
+    //document.write(points);
+    return points;		   
 }
+function getValuesDRD(nodeList){
+    var values=[];
+    
